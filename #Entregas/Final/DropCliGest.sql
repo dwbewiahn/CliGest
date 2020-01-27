1a93219be35a2073104fe6b20cdf36e5886b62f4
Douglas Bewiahn <dwbewiahn@users.noreply.github.com>
2020-01-27 14:35:25 +0000

    Add files via upload

---
+++ #Entregas/Final/DropCliGest.sql
@@ -0,0 +1,11 @@
+use XLuUja9EDK;
+
+SET FOREIGN_KEY_CHECKS = 0;
+drop table agendamento;
+drop table especialidade;
+drop table medico;
+drop table medico_has_especialidade;
+drop table paciente;
+drop table paciente_has_seguradora;
+drop table seguradora;
+SET FOREIGN_KEY_CHECKS = 1;
